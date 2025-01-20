4ffc3e44caab1bc0708ee6a686a87f45eb53ad38
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2025-01-20 00:16:01 +0000

    added UI for TodoItem and propeties

--- Pages/TodoPage/index.tsx
+++ Pages/TodoPage/index.tsx
@@ -5,19 +5,32 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import ButtonComponent from "../../components/ButtonComponent";
+import TodoItem from "../../components/TodoItem";
 
 const TodoPage = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  const [updateModalVisible, setUpdateModalVisible] = useState(false);
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
   return (
     <View style={styles.mainCntnr}>
-      <ButtonComponent
-        buttonName="Logout"
-        onPress={() => navigation.popTo("Login")}
-      />
+      <View>
+        <TodoItem
+          updateModalVisible={updateModalVisible}
+          setUpdateModalVisible={setUpdateModalVisible}
+          deleteModalVisible={deleteModalVisible}
+          setDeleteModalVisible={setDeleteModalVisible}
+        />
+      </View>
+      <View>
+        <ButtonComponent
+          buttonName="Logout"
+          onPress={() => navigation.popTo("Login")}
+        />
+      </View>
     </View>
   );
 };
